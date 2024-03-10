b518cb6006541c08f62d98b09fb9dd3e20ba29cf
rothgq <rothgq@gmail.com>
2024-03-10 01:39:47 -0500

    Update to readme and added the location for the README to generate.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # README-Generator
-A README generator for new projects.
+
+A professional README generator for new projects.
+
+## Navigation
+- [User-Story](#user-story)
+- [Description](#description)
+- [Author](#author)
+
+## User-Story
+
+AS A developer
+I WANT a README generator
+SO THAT I can quickly create a professional README for a new project
+
+## Description
+
+This is a node project to create professionally structured README.md files through a series of questions provided in the terminal through the Inquirer package. Answer accordingly to create your own README.md conveniently.
+
+## Author
+
+Gregory Quinn Roth - https://github.com/rothgq
--- index.js
+++ index.js
@@ -1,13 +1,72 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const path = require('path');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the intent of your project, how does it work, and/or what does it do?",
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "What dependancies are required for your project?",
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Which license is associated with this project?",
+        choices: ["Apache", "GNU v3.0", "MIT License", "Eclipse", "Boost Software License", "Mozilla Public License", "none"],
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Which languages or technologies were used with this project?",
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your Github username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your professional e-mail?",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "List all project contributors by Github username.",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "List any required tests for the project.",
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        writeToFile('./created/README.md', generateMarkdown({ ...responses }));
+        console.log('README.md generation in progress');
+    });
+}
 
 // Function call to initialize app
 init();
